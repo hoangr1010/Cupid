efd1e8ea3ee003c785438b9edb230d1c6e828009
lnguyen2693 <n9linh@gmail.com>
2024-03-30 23:07:26 -0400

    CRUD get user profile, add new experience

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -9,23 +9,18 @@ import {
 // create user profile -- POST
 export const createUser = async (req, res) => {
     try {
-        const { linkedin_id, email, first_name, last_name } = req.body;
-        const newUser = await User.create({ 
-            linkedin_id, 
-            email, 
-            first_name, 
-            last_name 
-        });
+        const data = req.body;
+        const newUser = await User.create(data);
 
         res.status(201).json({ 
             message: 'User created successfully', 
-            data: newUser 
+            data: newUser,
         });
 
     } catch (error) {
         res.status(400).json({ 
             message: 'Error creating course', 
-            error: error.message 
+            error: error.message,
         });
     }
 };
@@ -33,46 +28,79 @@ export const createUser = async (req, res) => {
 // get user profile -- GET
 export const getUser = async (req, res) => {
     try {
-        const { linkedin_id, email } = req.headers;
+        const { linkedin_id, email } = req.body;
         const user_profile = await User.findOne({ linkedin_id, email });
         res.status(201).json({ 
             message: 'User created successfully', 
-            data: user_profile 
+            data: user_profile,
         });
     } catch (error) {
         res.status(400).json({ 
             message: 'Error getting user profile', 
-            error: error.message 
+            error: error.message,
         });
     }
 }
 
 // add user experience -- POST
+// rethink the flow of this funcrtion
 export const addExperience = async (req, res) => {
     try {
-        const { position, organization, start_date, end_date } = req.body;
-        const newExperience = await UserExperience.create({ 
-            position, 
-            organization, 
-            start_date, 
-            end_date 
-        });
+        const { _id } = req.headers;
+        const data = req.body;
+        const newExp = await UserExperience.create(data);
+        
+        await User.updateOne(
+            { _id },
+            { $push: { experience: newExp } }
+        );
 
         res.status(201).json({ 
             message: 'Experience created successfully', 
-            data: newExperience
+            data: newExp,
         });
 
     } catch (error) {
         res.status(400).json({ 
             message: 'Error creating new experience', 
-            error: error.message 
+            error: error.message,
         });
     }
 }
 
 // edit user experience -- PUT
+export const editExperience = async (req, res) => {
+    try {
+        const { _id } = req.headers;
+        const data = req.body;
+
+        const newExperience = await UserExperience.findOneAndUpdate(
+            { _id },
+            data,
+        );
+
+        res.status(201).json({
+            message: 'Experience updated successfully',
+            data: newExperience,
+        });
+
+    } catch (error) {
+        res.status(400).json({
+            message: 'Error editing experience',
+            error: error.message,
+        });
+    }
+}
 
 // delete user experience -- DELETE
+// export const delExperience = async (req, res) => {
+//     try {
+//     } catch (error) {
+//         res.status(400).json({
+//             message: 'Error deleting experience',
+//             error: error.message,
+//         });
+//     }
+// }
 
 
--- server/routes/UserRouter.js
+++ server/routes/UserRouter.js
@@ -1,11 +1,15 @@
 import express from "express";
 import { 
     createUser, 
-    getUser 
+    getUser,
+    addExperience,
 } from "./../controllers/UserController.js"
 const UserRouter = express.Router();
 
 UserRouter.post("/create", createUser);
 UserRouter.get("/profile", getUser);
+
+// for testing 
+UserRouter.post("/profile/addExp", addExperience);
 
 export default UserRouter;
