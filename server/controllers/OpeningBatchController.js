efed5648c577f7851597396fda9ac46503898847
linhkhanhhoang <93673250+linhkhanhhoang@users.noreply.github.com>
2024-03-31 21:17:59 -0400

    Implement DELETE endpoint for OpeningBatch

--- server/controllers/OpeningBatchController.js
+++ server/controllers/OpeningBatchController.js
@@ -1,6 +1,6 @@
 import OpeningBatch from "../models/OpeningBatch.js";
 
-export const createOpeningtBatch = async (req, res) => {
+const createOpeningtBatch = async (req, res) => {
   try {
     const { batch_name } = req.body;
     const newOpeningBatch = await OpeningBatch.create({ batch_name });
@@ -8,4 +8,22 @@ export const createOpeningtBatch = async (req, res) => {
   } catch (error) {
     res.status(400).json({ message: 'Error creating Opening Batch', error: error.message });
   }
+};
+
+const deleteOpeningBatch = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deletedOpeningBatch = await OpeningBatch.findByIdAndDelete(id);
+    if (!deletedOpeningBatch) {
+      return res.status(404).json({ message: 'Opening Batch not found' });
+    }
+    res.status(200).json({ message: 'Opening Batch deleted successfully', data: deletedOpeningBatch });
+  } catch (error) {
+    res.status(400).json({ message: 'Error deleting Opening Batch', error: error.message });
+  }
+};
+
+module.exports = {
+  createOpeningtBatch,
+  deleteOpeningBatch
 };
--- server/routes/OpeningBatchRouter.js
+++ server/routes/OpeningBatchRouter.js
@@ -1,7 +1,9 @@
 import express from "express";
-import { createOpeningBatch } from "./../controllers/OpeningBatchController.js"
+import { createOpeningBatch, deleteOpeningBatch } from "./../controllers/OpeningBatchController.js";
+
 const OpeningBatchRouter = express.Router();
 
 OpeningBatchRouter.post("/create", createOpeningBatch);
+OpeningBatchRouter.delete('/delete/:id', deleteOpeningBatch);
 
 export default OpeningBatchRouter;
