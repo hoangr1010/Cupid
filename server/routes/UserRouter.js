957700be6754d3aaf3df1010bfef46152bda8506
lnguyen2693 <n9linh@gmail.com>
2024-04-01 19:44:34 -0400

    delete unecessary codes

--- server/routes/UserRouter.js
+++ server/routes/UserRouter.js
@@ -1,13 +1,12 @@
 import express from "express";
 import { 
     createUser, 
-    getUser,
+    // getUser,
     addExperience,
 } from "./../controllers/UserController.js"
 const UserRouter = express.Router();
 
 UserRouter.post("/create", createUser);
-UserRouter.get("/profile", getUser);
 
 // for testing 
 UserRouter.post("/profile/addExp", addExperience);
