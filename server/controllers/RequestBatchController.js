ad84f972688f0423a74580f85158380ef1e06bd9
Khang Huu Nguyen <140891599+khangnh2005@users.noreply.github.com>
2024-04-02 17:23:00 -0400

    Finish GET route for request batch

--- server/controllers/RequestBatchController.js
+++ server/controllers/RequestBatchController.js
@@ -1,11 +1,21 @@
 import RequestBatch from "../models/RequestBatch.js";
 
+export const getRequestBatch = async (req, res) => {
+  try {
+    const { request_id } = req.params;
+    const batch = await RequestBatch.find({ _id: request_id }).exec();
+    res.status(201).json({ message: 'Successfully access Request Batch', data: batch });
+  } catch (error) {
+    res.status(400).json({ message: 'Error accessing Request Batch', error: error.message });
+  }
+};
+
 export const createRequestBatch = async (req, res) => {
   try {
     const { batch_name } = req.body;
-    const newRequestBatch = await RequestBatch.create({ batch_name });
-    res.status(201).json({ message: 'Request Batch created successfully', data: newRequestBatch });
+    const newBatch = await RequestBatch.create({ batch_name });
+    res.status(201).json({ message: 'Successfully created Request Batch', data: newBatch });
   } catch (error) {
     res.status(400).json({ message: 'Error creating Request Batch', error: error.message });
   }
-};
+}
--- server/routes/RequestBatchRouter.js
+++ server/routes/RequestBatchRouter.js
@@ -1,7 +1,8 @@
 import express from "express";
-import { createRequestBatch } from "./../controllers/RequestBatchController.js"
+import { createRequestBatch, getRequestBatch } from "./../controllers/RequestBatchController.js"
 const RequestBatchRouter = express.Router();
 
+RequestBatchRouter.get("/:request_id", getRequestBatch);
 RequestBatchRouter.post("/create", createRequestBatch);
 
 export default RequestBatchRouter;
