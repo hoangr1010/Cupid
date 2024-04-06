4ade511d68db208c491ccb2b836df15234791317
Khang Huu Nguyen <140891599+khangnh2005@users.noreply.github.com>
2024-04-06 17:15:37 -0400

    Add main content for referral request dashboard

--- client/src/pages/RequestDashboardPage/index.jsx
+++ client/src/pages/RequestDashboardPage/index.jsx
@@ -5,8 +5,8 @@ const RequestDashboardPage = () => {
     <div className="flex">
       <Sidebar />
 
-      <main className="flex-1 flex flex-col items-center">
-        <h1>Referral Request</h1>
+      <main className="flex-1 flex flex-col items-center p-4">
+        <h1 className="text-3xl">Referral Request</h1>
       </main>
     </div>
   );
