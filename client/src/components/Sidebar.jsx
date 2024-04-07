766e68bf91a325b19fdd396e45d7afacb1bfd20f
lnguyen2693 <n9linh@gmail.com>
2024-04-07 08:10:45 -0400

    navigate to proper page

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -4,9 +4,9 @@ const Sidebar = () => {
   return (
     <ul className="basis-60 flex-initial h-screen flex flex-col gap-3 p-4 bg-gray-300 shadow-2xl">
       <div className="text-3xl m-2">❤️ Cupid</div>
-      <SidebarItem text="Profile" />
-      <SidebarItem text="Referral Request" />
-      <SidebarItem text="Referral Opening" />
+      <SidebarItem path="/profile" text="Profile" />
+      <SidebarItem path="/profile" text="Referral Request" />
+      <SidebarItem path="/profile" text="Referral Opening" />
     </ul>
   );
 };
