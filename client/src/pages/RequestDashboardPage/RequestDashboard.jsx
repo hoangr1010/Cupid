855fbe24818e2909a103e1c437198cfdb2003210
Khang Huu Nguyen <140891599+khangnh2005@users.noreply.github.com>
2024-04-11 22:07:34 -0400

    Make UI for RequestDashboardPage

--- client/src/pages/RequestDashboardPage/RequestDashboard.jsx
+++ client/src/pages/RequestDashboardPage/RequestDashboard.jsx
@@ -1,5 +1,23 @@
+import RequestCard from "./RequestCard";
+
 const RequestDashboard = () => {
-  
+  return (
+    <main className="flex-1 flex flex-col items-center gap-12 py-7 px-12 overflow-auto">
+      <h1 className="text-3xl">Referral Request</h1>
+
+      <div className="w-full grid grid-cols-2 gap-10">
+        <RequestCard company="Google" priority={1} status="waiting"/>
+        <RequestCard company="Amazon" priority={1} status="waiting"/>
+        <RequestCard company="Meta" priority={1} status="approved"/>
+        <RequestCard company="LinkedIn" priority={1} status="referred"/>
+        <RequestCard company="Netflix" priority={1} status="waiting"/>
+        <RequestCard company="Microsoft" priority={1} status="matched"/>
+        <RequestCard company="DoorDash" priority={1} status="waiting"/>
+        <RequestCard company="Vanta" priority={1} status="referred"/>
+        <RequestCard company="Tesla" priority={1} status="waiting"/>
+      </div>
+    </main>
+  );
 };
 
 export default RequestDashboard;
--- client/src/pages/RequestDashboardPage/index.jsx
+++ client/src/pages/RequestDashboardPage/index.jsx
@@ -1,13 +1,11 @@
 import Sidebar from "../../components/Sidebar";
+import RequestDashboard from "./RequestDashboard";
 
 const RequestDashboardPage = () => {
   return (
-    <div className="flex">
+    <div className="flex h-screen">
       <Sidebar />
-
-      <main className="flex-1 flex flex-col items-center p-4">
-        <h1 className="text-3xl">Referral Request</h1>
-      </main>
+      <RequestDashboard />
     </div>
   );
 };
