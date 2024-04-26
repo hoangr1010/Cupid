6d4e59cea0bb7026f3d9d19c83af4d8beb0a206d
Nguyen Huu Huy Hoang <nguyenh226@mymacewan.ca>
2024-04-25 21:20:49 -0600

    test server CICD

--- server/app.js
+++ server/app.js
@@ -20,6 +20,9 @@ const createApp = () => {
   app.use("/auth", AuthRouter);
   app.use("/request", requestRouter);
   app.use("/opening", OpeningRouter);
+  app.get("/", (req, res) => {
+    res.send("Hello from the API");
+  })
 
   return app;
 };
