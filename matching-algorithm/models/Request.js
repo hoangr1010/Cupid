c43bfd3bb382e3ff88aaad76f86892507decad59
Khang Huu Nguyen <140891599+khangnh2005@users.noreply.github.com>
2024-05-01 02:10:03 -0400

    Implement matching algorithm based on scale

---
+++ matching-algorithm/models/Request.js
@@ -0,0 +1,42 @@
+import mongoose from "mongoose";
+const { Schema } = mongoose;
+
+// request made by candidate
+const requestSchema = new Schema(
+  {
+    candidate_id: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+    opening_id: {
+      type: Schema.Types.ObjectId,
+      required: function () {
+        return this.status !== "waiting";
+      },
+      ref: "Opening",
+    },
+    company: {
+      type: String,
+      required: true,
+    },
+    priority: {
+      type: Number,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ["waiting", "matched", "approved", "referred"],
+      required: true,
+    },
+    scale: {
+      type: Number,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export default mongoose.model("Request", requestSchema, "Requests");
