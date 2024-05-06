b55764ebf26d8fb2e199ae0f1e8b28c12db20c22
linhkhanhhoang <93673250+linhkhanhhoang@users.noreply.github.com>
2024-05-06 18:44:14 -0400

    FInished creating table view for request

--- client/src/pages/RequestDashboardPage/RequestCard.jsx
+++ client/src/pages/RequestDashboardPage/RequestCard.jsx
@@ -1,25 +1,44 @@
-import { BsThreeDots } from "react-icons/bs";
+import { Badge } from "flowbite-react";
 
-const RequestCard = ({ company, status, priority }) => {
+const RequestCard = ({ status }) => {
+  const badgeColor = {
+    waiting: "warning",
+    matched: "info",
+    approved: "success",
+    referred: "purple",
+  };
+
   return (
-    <div className="widget_container flex justify-between">
-      <div className="flex flex-col gap-3">
-        <div className="text-2xl flex items-center gap-2">
-          <span className="inline-block w-8 h-8 border-2 border-black rounded-full flex justify-center items-center">
-            {priority}
-          </span>
-          {company}
-        </div>
-
-        <div>
-          <div>Status: {status}</div>
-          <div>Job Posting: </div>
-        </div>
-      </div>
-
-      <BsThreeDots />
-    </div>
+    <Badge color={badgeColor[status]} size="sm">
+      {status}
+    </Badge>
   );
 };
 
 export default RequestCard;
+
+// import { BsThreeDots } from "react-icons/bs";
+
+// const RequestCard = ({ company, status, priority }) => {
+//   return (
+//     <div className="widget_container flex justify-between">
+//       <div className="flex flex-col gap-3">
+//         <div className="text-2xl flex items-center gap-2">
+//           <span className="inline-block w-8 h-8 border-2 border-black rounded-full flex justify-center items-center">
+//             {priority}
+//           </span>
+//           {company}
+//         </div>
+
+//         <div>
+//           <div>Status: {status}</div>
+//           <div>Job Posting: </div>
+//         </div>
+//       </div>
+
+//       <BsThreeDots />
+//     </div>
+//   );
+// };
+
+// export default RequestCard;
