6818d6902459fa9b22bec46cac13000da3d3ec63
Nguyen Huu Huy Hoang <nguyenh226@mymacewan.ca>
2024-05-31 20:32:18 +0700

    polish homepage

--- client/src/pages/HomePage/index.jsx
+++ client/src/pages/HomePage/index.jsx
@@ -2,7 +2,14 @@ import React from "react";
 import StatisticsTable from "./StatisticsTable.jsx";
 
 const HomePage = () => {
-  return <StatisticsTable />;
+  return (
+    <>
+      <h1 className="text-5xl font-bold font-darker mb-8 text-primaryDark">
+        Dashboard
+      </h1>
+      <StatisticsTable />
+    </>
+  );
 };
 
 export default HomePage;
