0e2b20bd0b08c545b7e1b65d1448031ec60e51b0
lnguyen2693 <n9linh@gmail.com>
2024-06-02 10:55:20 -0600

    reduce calling API

--- client/src/api/request.js
+++ client/src/api/request.js
@@ -2,6 +2,7 @@ import API from ".";
 import { toast } from "sonner";
 import { changeRequestList } from "../state";
 import { validateFileName } from "../utils/request";
+import { changeOneRequest } from "../state";
 
 export const getAllRequests = async (userId) => {
   try {
@@ -51,10 +52,9 @@ export const sendFile = async (request, name, file, dispatch) => {
     // validate if name is in redux
     validateFileName(request, name);
 
-    await API.post(`request/upload`, formData);
-    const response = await getAllRequests(request.request.request.candidate_id);
-    dispatch(changeRequestList(response.data.data));
+    const response = await API.post(`request/upload`, formData);
 
+    dispatch(changeOneRequest(response.data.data));
     toast.success("Upload file successfully");
   } catch (error) {
     toast.error("Error uploading file: " + error);
@@ -63,14 +63,14 @@ export const sendFile = async (request, name, file, dispatch) => {
 
 export const delFile = async (path, dispatch) => {
   try {
-    await API.post(`request/del`, { path });
-    const response = await getAllRequests(path.split("/")[0]);
-    dispatch(changeRequestList(response.data.data));
+    const response = await API.post(`request/del`, { path });
+
+    dispatch(changeOneRequest(response.data.data));
     toast.success("Delete file successfully");
   } catch (error) {
     toast.error("Error deleting file: " + error);
   }
-}
+};
 
 export const getAllExistingRequests = async () => {
   try {
--- server/src/controllers/Request.js
+++ server/src/controllers/Request.js
@@ -231,14 +231,18 @@ export const updateFile = async (req, res) => {
 export const deleteFile = async (req, res) => {
   try {
     const { path } = req.body;
-    
+
     const request_id = path.split("/")[2];
     const request = await Request.findById(request_id);
-    
+
     const new_request_files = request.request_files.filter((e) => e != path);
-    const data = await Request.findByIdAndUpdate(request_id, {
-      request_files: new_request_files,
-    });
+    const data = await Request.findByIdAndUpdate(
+      request_id,
+      {
+        request_files: new_request_files,
+      },
+      { new: true },
+    );
 
     res.status(200).json({
       message: "Delete file successfully",
