92ffc9d949f7987895810e4eb87df3e67a47a83e
lnguyen2693 <n9linh@gmail.com>
2024-06-17 05:40:10 -0600

    test

--- server/src/routes/User.js
+++ server/src/routes/User.js
@@ -10,6 +10,9 @@ import { checkUserId, updateResumeText } from "../middleware/User.js";
 import { verifyToken } from "../middleware/Auth.js";
 
 import { loadResumeToS3 } from "../middleware/fileHandle.js";
+// TEST
+// import { Notification } from "../utils/notification.js";
+// import { sendNoti } from "../services/Notification/notification.js";
 
 const UserRouter = express.Router();
 const upload = multer({ storage: multer.memoryStorage() });
@@ -27,5 +30,26 @@ UserRouter.post(
 );
 
 UserRouter.put("/addEducation", checkUserId, verifyToken, addEducation);
+
+// TEST
+// UserRouter.post("/test", async (req, res) => {
+//   try {
+//     // make Notification 
+//     const noti = Notification.requestRemindAction("requestRemindAction", "requestId_whatever_bo gi vao cx dc :D");
+//     console.log(noti);
+
+//     // send Notification data to the notification microservice
+//     await sendNoti(noti);
+    
+//     res.status(201).json({
+//       message: "success",
+//     });
+//   } catch (error) {
+//     res.status(400).json({
+//       message: "Error",
+//       error: error.message,
+//     });
+//   }
+// })
 
 export default UserRouter;
