cd6b06ab6eb2ec6e0fe431077672b16d31595d35
Khang Huu Nguyen <140891599+khangnh2005@users.noreply.github.com>
2024-06-19 17:34:51 +0700

    Made handler for use in aws lambda

--- notification-service/worker/index.js
+++ notification-service/worker/index.js
@@ -1,19 +1,17 @@
 import dotenv from "dotenv";
 dotenv.config();
-// Load the AWS SDK for Node.js
 import AWS from "aws-sdk";
+import db from "../utils/connectDB.js";
 
-// Set the region
 AWS.config.update({
   region: "us-west-1",
 });
 
-// Create an SQS service object
-var sqs = new AWS.SQS({ apiVersion: "2012-11-05" });
+const sqs = new AWS.SQS({ apiVersion: "2012-11-05" });
 
-var queueURL = process.env.SQS_QUEUE_URL;
+const queueURL = process.env.SQS_QUEUE_URL;
 
-var queueParams = {
+const queueParams = {
   AttributeNames: ["SentTimestamp"],
   MaxNumberOfMessages: 10,
   MessageAttributeNames: ["All"],
@@ -22,54 +20,47 @@ var queueParams = {
   WaitTimeSeconds: 0,
 };
 
-// Create sendEmail params
-var emailParams = {
-  Destination: {
-    /* required */
-    ToAddresses: [
-      "huyhoangr1010@gmail.com",
-      /* more items */
-    ],
-  },
-  Message: {
-    /* required */
-    Body: {
-      /* required */
-      Text: {
-        Charset: "UTF-8",
-        Data: "Email Body",
-      },
-    },
-    Subject: {
-      Charset: "UTF-8",
-      Data: "SES Subject",
-    },
-  },
-  Source: "hiwecupid@gmail.com" /* required */,
-  ReplyToAddresses: [
-    "hiwecupid@gmail.com",
-    /* more items */
-  ],
-};
-
 sqs.receiveMessage(queueParams, function (err, data) {
   if (err) {
     console.log("Receive Error", err);
   } else if (data.Messages) {
     // Do something with the some data at the front of the queue
-    // Create the promise and SES service object
-    let sendPromise = new AWS.SES({ apiVersion: "2010-12-01" })
-      .sendEmail(emailParams)
-      .promise();
+    const asyncDoAll = [];
+    for (let message of data.Messages) {
+      const obj = JSON.parse(message.Body);
+      var emailParams = {
+        Destination: {
+          ToAddresses: ["test@deadapple.simplelogin.com"],
+        },
+        Message: {
+          Body: {
+            Text: {
+              Charset: "UTF-8",
+              Data: JSON.stringify(obj),
+            },
+          },
+          Subject: {
+            Charset: "UTF-8",
+            Data: "SES Subject",
+          },
+        },
+        Source: "hiwecupid@gmail.com",
+        ReplyToAddresses: ["hiwecupid@gmail.com"],
+      };
 
-    // Handle promise's fulfilled/rejected states
-    sendPromise
-      .then(function (data) {
-        console.log(data.MessageId);
-      })
-      .catch(function (err) {
-        console.error(err, err.stack);
-      });
+      const sendPromise = new AWS.SES({ apiVersion: "2010-12-01" })
+        .sendEmail(emailParams)
+        .promise();
+      asyncDoAll.push(sendPromise);
+
+      const docRef = db.collection("notifications").doc();
+      const saveNotification = docRef.set(obj);
+      asyncDoAll.push(saveNotification);
+    }
+
+    Promise.all(asyncDoAll).then((values) => {
+      console.log(values);
+    });
 
     // Pop data off the queue
     var deleteParams = {
