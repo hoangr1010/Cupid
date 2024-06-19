16e1163ec5d69d7e576bfc656ab9847f1be5f435
Khang Huu Nguyen <140891599+khangnh2005@users.noreply.github.com>
2024-06-19 20:45:02 +0700

    Add space for readability

--- server/matching-algorithm/scale/scale.js
+++ server/matching-algorithm/scale/scale.js
@@ -35,10 +35,13 @@ const priorityScale = (priority) => {
 
 const compatibilityScale = async (request) => {
   const jobPostingText = await extractJobPostingText(request.job_posting_url);
+
   const resumeText = (
     await Request.findById(request._id).populate("candidate_id").exec()
   ).candidate_id.resume.text;
+
   const compatibility = await compatibilityFunction(jobPostingText, resumeText);
+
   const hourMilliSeconds = 3600000;
   return hourMilliSeconds * compatibility;
 };
