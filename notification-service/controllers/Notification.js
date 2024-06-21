27cb52fe578b3edada54ac7def9deb40ab3e88cc
lnguyen2693 <n9linh@gmail.com>
2024-06-21 07:58:46 -0400

    add field to Notification object

---
+++ notification-service/controllers/Notification.js
@@ -0,0 +1,15 @@
+import db from "../utils/connectDB.js"
+
+export const changeSeenField = async (req, res) => {
+  try {
+    // get Notification object
+    // const notiRef = db.collection()
+
+    // change seen field
+
+    // return new doc
+
+  } catch (error) {
+    
+  }
+}
--- notification-service/microservice/Notification.js
+++ notification-service/microservice/Notification.js
@@ -8,6 +8,7 @@ export const notificationService = (req, res) => {
       requestId: req.body.requestId,
       openingId: req.body.openingId,
       userPref: req.body.userPref,
+      seen: false,
       retrySend: 0,
     }
   
