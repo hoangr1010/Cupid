d6700e03a31966af883d7c6d2ed03a7c6f7ef1b9
Nguyen Huu Huy Hoang <nguyenh226@mymacewan.ca>
2024-06-23 12:05:29 +0700

    quick refer button for approve request in table

--- client/src/pages/OpeningDashboardPage/TableView/QuickAction.jsx
+++ client/src/pages/OpeningDashboardPage/TableView/QuickAction.jsx
@@ -1,9 +1,33 @@
-import React from "react";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
 import { IoMdMore } from "react-icons/io";
 import { Dropdown } from "flowbite-react";
+import { changeRequestStatusInOpening } from "../../../state";
+import { changeStatus } from "../../../api/opening";
 
 const QuickAction = ({ request }) => {
+  const dispatch = useDispatch();
   const status = request.status;
+  const requestId = request._id
+  const [isButtonLoading, setIsButtonLoading] = useState(false);
+
+  const changeRequestStatus = async (newStatus) => {
+
+    if (isButtonLoading) {
+      return;
+    }
+
+    setIsButtonLoading(true);
+    const formData = { requestId, newStatus };
+    const newRequest = await changeStatus(formData);
+
+    if (newRequest) {
+      const requestId = newRequest._id;
+      const newStatus = newRequest.status;
+      dispatch(changeRequestStatusInOpening({ requestId, newStatus }));
+    }
+    setIsButtonLoading(false);
+  };
 
   return (
     <div>
@@ -19,13 +43,14 @@ const QuickAction = ({ request }) => {
       >
         {status === "approved" && (
           <>
-            <Dropdown.Item>
+            <Dropdown.Item onClick={() => {
+              changeRequestStatus("referred");
+            }}>
               <p className="font-bold text-xs">Refer Candidate</p>
             </Dropdown.Item>
             <Dropdown.Item>
               <p className="font-bold text-xs">Request Infomation</p>
             </Dropdown.Item>
-
           </>
         )}
       </Dropdown>
